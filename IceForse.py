a9c0fb4d552d9108aa4a0263978e3386d83bcd58
Bezruk George <remix007@mail.ru>
2025-06-03 13:45:47 +1000

    <Программа расчёт ледовой нагрузки>

---
+++ IceForse.py
@@ -0,0 +1,126 @@
+# Оконный интерфейс (tkinter)
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+import table_data_sp
+import function_iceforce
+
+# Вызов общего окна
+window = Tk()
+window.title("Расчёт ледовой нагрузки от полей ровного льда")
+window.geometry("1100x950")
+
+# Вывод заголовка в окне
+zag = Label(
+    window,
+    text="Ввод исходных данных:",
+    font=("Arial Bold", 10),
+)
+zag.grid(column=0, row=0)
+
+# Блок подписей вводимых переменных (column 0)
+
+speedtxt = Label(
+    window,
+    text="Скорость движения ледяного поля, ",
+    font=("Arial Bold", 10),
+)  # Текст скорости движения льда
+speedtxt.grid(column=0, row=1, sticky="w")
+
+hdtxt = Label(
+    window,
+    text="Толщина ровного льда, ",
+    font=("Arial Bold", 10),
+)  # Текст ввода толщины льда
+hdtxt.grid(column=0, row=2, sticky="w")
+
+formtxt = Label(
+    window,
+    text="Форма сооружения в плане: ",
+    font=("Arial Bold", 10),
+)  # Текст ввода формы сооружжения в плане
+formtxt.grid(column=0, row=3, sticky="w")
+
+angletxt = Label(
+    window,
+    text="Угол заострения треугольного сооружения в плане",
+    font=("Arial Bold", 10),
+)  # Угол заострения опоры в плане_подпись
+angletxt.grid(column=0, row=4, sticky="w")
+
+# Ввод переменных (column 1)
+speedobz = Label(
+    window,
+    text="V = ",
+    font=("Arial Bold", 10),
+)  # Текст ввода обозначения скорости
+speedobz.grid(column=1, row=1, sticky="e")
+
+hdobz = Label(
+    window,
+    text="hd = ",
+    font=("Arial Bold", 10),
+)  # Текст ввода обозначения толщины льда
+hdobz.grid(column=1, row=2, sticky="e")
+
+angleobz = Label(
+    window,
+    text="2γ = ",
+    font=("Arial Bold", 10),
+)  # Текст обозначения угла сооружения в плане
+angleobz.grid(column=1, row=4, sticky="e")
+
+
+# Блок вывода единиц измерения (column 3)
+ms = "м/с"  # Единицы измерения м/с
+m = "м"
+edmm1 = Label(window, text=ms, font=("Arial Bold", 10))
+edmm1.grid(column=3, row=1)
+edmm2 = Label(window, text=m, font=("Arial Bold", 10))
+edmm2.grid(column=3, row=2)
+edmm_angle = Label(window, text="°", font=("Arial Bold", 10))
+edmm_angle.grid(column=3, row=4)
+
+# Блок вспомогательных подписей (column 4)
+# Подписи расчёта размеров краевых полей
+koef_m_result = Label(
+    window, text="Коэффициент формы опоры в плане, m = ", font=("Arial Bold", 10)
+)
+koef_m_result.grid(column=0, row=5, columnspan=3, sticky="w")
+
+
+# Блок полей ввода (column 2)
+
+v_speed = Entry(window, width=10)  # окно ввода текста - скорость льда
+v_speed.grid(column=2, row=1)  # положение окна ввода текста - скорость льда
+
+hd = Entry(window, width=10)  # окно ввода текста - скорость льда
+hd.grid(column=2, row=2)  # положение окна ввода текста - скорость льда
+
+forms = [
+    "Треугольник",
+    "Многогранник/полуциркульное",
+    "Прямоугольник",
+]
+cbox_form = ttk.Combobox(values=forms)
+cbox_form.grid(column=1, row=3, columnspan=3)
+cbox_form.bind(
+    "<<ComboboxSelected>>",
+    lambda event: function_iceforce.koef_m(
+        event, forms, cbox_form, koef_m_result, angle
+    ),
+)
+
+angle = Entry(
+    window, width=10, state="readonly"
+)  # окно ввода текста - угол заострения соружения
+angle.grid(column=2, row=4)
+angle.bind(
+    "<FocusOut>",
+    lambda e: function_iceforce.m_triang_interpolation(
+        table_data_sp.m_triang, angle.get(), koef_m_result
+    ),
+)
+
+
+window.mainloop()  # бесконечный цикл, чтобы окно не закрывалось
---
+++ function_iceforce.py
@@ -0,0 +1,26 @@
+import table_data_sp
+import numpy as np
+
+
+# Функция интерполяции значений m, если выбрана форма треугольник
+def m_triang_interpolation(table_m_triang, angle, koef_m_result):
+    angle_values = np.array(table_m_triang[0])  # Значения угла табличные
+    m_values = np.array(table_m_triang[1])  # Значения m табличные
+    m = np.interp(float(angle), angle_values, m_values)  # Интерполяция значения
+    koef_m_str = f"Коэффициент формы опоры в плане, m = {m}"
+    koef_m_result.config(text=koef_m_str)  # Вспомогательная подпись для обозначения
+
+
+# Функция присваивания m при выборе прямоугольной формы и многогранника
+def koef_m(event, forms, cbox_form, koef_m_result, angle):
+    form_var = cbox_form.get()
+    if form_var == forms[2]:  # Прямоугольник
+        m = table_data_sp.m_sqad
+    elif form_var == forms[1]:  # Многогранник
+        m = table_data_sp.m_mnog
+    elif form_var == forms[0]:  # Треугольник
+        angle.config(state="normal")
+        return  # Не обновляем m, пока не введён угол
+
+    koef_m_str = f"Коэффициент формы опоры в плане, m = {m}"
+    koef_m_result.config(text=koef_m_str)
